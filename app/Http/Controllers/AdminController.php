75015fbb5d6771177f1f398c2598da14954cf096
kimp <ahmadmiqdad27@gmail.com>
2024-12-23 03:32:03 +0700

    Adding dashboard for each role

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,26 +9,41 @@ class AdminController extends Controller
 {
     function index()
     {
-        return view('dashboard');
+        // return view('dashboard');
+        $role = Auth::user()->role;
+
+        switch ($role) {
+            case 'sm':
+                return redirect('/dashboard/admin');
+            case 'user':
+                return redirect('/dashboard/user');
+            case 'supplier':
+                return redirect('/dashboard/supplier');
+            default:
+                abort(403, 'Unauthorized');
+        }
     }
     function admin()
     {
-        echo "<h1> Hello, " . Auth::user()->name . "</h1>";
-        echo "<h1>Ini Halaman Admin</h1>";
-        echo "<a href='/logout'>Logout</a>"; // Arahkan ke /logout
+        // echo "<h1> Hello, " . Auth::user()->name . "</h1>";
+        // echo "<h1>Ini Halaman Admin</h1>";
+        // echo "<a href='/logout'>Logout</a>"; // Arahkan ke /logout
+        return view('adminDashboard');
     }
 
     function user()
     {
-        echo "<h1> Hello, " . Auth::user()->name . "</h1>";
-        echo "<h1>Ini Halaman User</h1>";
-        echo "<a href='/logout'>Logout</a>"; // Arahkan ke /logout
+        // echo "<h1> Hello, " . Auth::user()->name . "</h1>";
+        // echo "<h1>Ini Halaman User</h1>";
+        // echo "<a href='/logout'>Logout</a>"; // Arahkan ke /logout
+        return view('userDashboard');
     }
 
     function supplier()
     {
-        echo "<h1> Hello, " . Auth::user()->name . "</h1>";
-        echo "<h1>Ini Halaman Supplier</h1>";
-        echo "<a href='/logout'>Logout</a>"; // Arahkan ke /logout
+        // echo "<h1> Hello, " . Auth::user()->name . "</h1>";
+        // echo "<h1>Ini Halaman Supplier</h1>";
+        // echo "<a href='/logout'>Logout</a>"; // Arahkan ke /logout
+        return view('supplierDashboard');
     }
 }
