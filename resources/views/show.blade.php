f4005edaf3daa88103db0a65657b2d292ca3b25b
kimp <ahmadmiqdad27@gmail.com>
2025-01-05 16:24:25 +0700

    Moving from barang to wr

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,14 +4,13 @@
 {{-- @section('title', 'Admin Dashboard') --}}
 
 @section('content')
-
     <div class="container mt-5 mb-5">
         <div class="row">
             <div class="col-md-8">
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                        <p>Nama Barang : {{ $barangs->barang }}</p>
-                        <p>Jumlah : {{ $barangs->jumlah }}</p>
+                        <p>Nama Barang : {{ $wr->dstrc_ori }}</p>
+                        <p>Jumlah : {{ $wr->qty_req }}</p>
                     </div>
                 </div>
             </div>
@@ -19,4 +18,4 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    @endsection
+@endsection
